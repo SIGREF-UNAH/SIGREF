ece927a8344b670d14463666f0ce0892ed73ec11
Anthony Edward Miranda Fuentes <157448636+AnthonyEMF@users.noreply.github.com>
2025-09-15 21:36:53 -0600

    Creating the Rest API Project with ASP.NET (#12)
    * Creating the Rest API Project with ASP.NET
    
    - Initial project setup
    - Folders structure
    - Adding the .gitignore file
    - Adding the EntityFramework, AutoMapper, Newtonsoft and JWT dependencies
    
    * Removing authentication settings

---
+++ BE/SIGREF.API/Database/SIGREFContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SIGREF.API.Database
+{
+    public class SIGREFContext : DbContext
+    {
+        public SIGREFContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ BE/SIGREF.API/Startup.cs
@@ -0,0 +1,52 @@
+﻿namespace SIGREF.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddHttpContextAccessor();
+
+            // CORS Configuration
+            services.AddCors(opt =>
+            {
+                var allowURLS = _configuration.GetSection("AllowURLS").Get<string[]>();
+
+                opt.AddPolicy("CorsPolicy", builder => builder
+                .WithOrigins(allowURLS)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
