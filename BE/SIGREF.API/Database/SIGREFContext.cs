d8a4ffcc2fbf2d75f5d69207f38d3e2e1af23796
AnthonyEMF <anthony07miranda@gmail.com>
2025-09-15 21:27:11 -0600

    Removing authentication settings

--- BE/SIGREF.API/Database/SIGREFContext.cs
+++ BE/SIGREF.API/Database/SIGREFContext.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SIGREF.API.Database
 {
-    public class SIGREFContext : IdentityDbContext<IdentityUser>
+    public class SIGREFContext : DbContext
     {
         public SIGREFContext(DbContextOptions options) : base(options)
         {
-            
         }
     }
 }
--- BE/SIGREF.API/Startup.cs
+++ BE/SIGREF.API/Startup.cs
@@ -1,18 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using SIGREF.API.Database;
-using System.Text;
-
-namespace SIGREF.API
+﻿namespace SIGREF.API
 {
     public class Startup
     {
-        private IConfiguration Configuration { get; }
+        private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this._configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -22,36 +16,10 @@ namespace SIGREF.API
             services.AddSwaggerGen();
             services.AddHttpContextAccessor();
 
-            var name = Configuration.GetConnectionString("DefaultConnection");
-
-            // Add DbContext
-            services.AddDbContext<SIGREFContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = false,
-                    ValidAudience = Configuration["JWT:ValidAudience"],
-                    ValidIssuer = Configuration["JWT:ValidIssuer"],
-                    ClockSkew = TimeSpan.Zero,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-                };
-            });
-
             // CORS Configuration
             services.AddCors(opt =>
             {
-                var allowURLS = Configuration.GetSection("AllowURLS").Get<string[]>();
+                var allowURLS = _configuration.GetSection("AllowURLS").Get<string[]>();
 
                 opt.AddPolicy("CorsPolicy", builder => builder
                 .WithOrigins(allowURLS)
@@ -74,10 +42,6 @@ namespace SIGREF.API
             app.UseRouting();
 
             app.UseCors("CorsPolicy");
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
