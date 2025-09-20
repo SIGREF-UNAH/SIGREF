6065aa4512a7be14ce20df9bbdf316f96fb1bad4
AnthonyEMF <anthony07miranda@gmail.com>
2025-09-19 19:13:47 -0600

    Se creó la LoginPage y su ruta
    - Se movió el ejemplo de uso de keycloak a la LoginPage
    - Se agregó la ruta /login en el AuthRouter

--- FE/src/config/keycloak.ts
+++ FE/src/config/keycloak.ts
@@ -3,6 +3,6 @@ import Keycloak from "keycloak-js";
 export const keycloak = new Keycloak({
   url: "http://localhost:8081/", 
   realm: "react-auth",            
-  clientId: "React-Auth",   
+  clientId: "react-auth",   
 });
 
--- FE/src/features/auth/routers/AuthRouter.tsx
+++ FE/src/features/auth/routers/AuthRouter.tsx
@@ -1,37 +1,10 @@
-import { useKeycloak } from "@react-keycloak/web";
+import { Route, Routes } from "react-router";
+import { LoginPage } from "../pages";
 
 export const AuthRouter = () => {
-  
-  // TEST DE KEYCLOAK
-  const { keycloak, initialized } = useKeycloak();
-
-  if (!initialized) {
-    return <div>Cargando autenticación...</div>;
-  }
-
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-500">
-      {!keycloak.authenticated ? (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => keycloak.login()}
-        >
-          Iniciar sesión
-        </button>
-      ) : (
-        <div className="text-center">
-          <h1 className="text-3xl">
-            Autenticación exitosa con Keycloak <br/>
-            Bienvenido {keycloak.tokenParsed?.preferred_username}
-          </h1>
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white mt-5 font-bold py-2 px-4 rounded"
-            onClick={() => keycloak.logout()}
-          >
-            Cerrar sesión
-          </button>
-        </div>
-      )}
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 };
