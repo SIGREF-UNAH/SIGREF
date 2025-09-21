d14ad9fad16719de7f43251d9bc36612e19f2e54
AnthonyEMF <anthony07miranda@gmail.com>
2025-09-20 22:59:23 -0600

    Manejo de variables de entorno
    - Se creo el archivo .env para configurar variables de entorno
    - Se creo el archivo .env.template como guia
    - La configuracion de keycloak ahora es con .env

--- FE/src/config/keycloak.ts
+++ FE/src/config/keycloak.ts
@@ -1,8 +1,7 @@
 import Keycloak from "keycloak-js";
 
 export const keycloak = new Keycloak({
-  url: "http://localhost:8081/", 
-  realm: "react-auth",            
-  clientId: "react-auth",   
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
 });
-
