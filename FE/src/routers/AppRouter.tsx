befe54d3fd7e093aec34c64ce1b2733393316889
Ever Garcia <161626404+everjosue56@users.noreply.github.com>
2025-09-22 18:43:39 -0600

    Trabajo en progreso (#39)
    * Trabajo en progreso
    
    * Add @ant-design/v5-patch-for-react-19 to dependencies and update AuthRouter import
    
    ---------
    
    Co-authored-by: erickArita <erick.emao@gmail.com>

--- FE/src/routers/AppRouter.tsx
+++ FE/src/routers/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router";
-import { AuthRouter } from "../features/auth/routers";
+import AuthRouter from "../features/auth/routers/AuthRouter";
 
 export const AppRouter = () => {
   return (
