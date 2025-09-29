22969413843e121d18cbb215ed185253f02adaa5
erickArita <erick.emao@gmail.com>
2025-09-28 22:47:45 -0600

    feat(auth): implementar autenticación con Keycloak en la aplicación

--- FE/src/routers/AppRouter.tsx
+++ FE/src/routers/AppRouter.tsx
@@ -3,10 +3,6 @@ import AuthRouter from "../features/auth/routers/AuthRouter";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route>   
-        <Route path="/*" element={<AuthRouter />} />
-        </Route>  
-    </Routes>
+   "hola mundo"
   );
 };  
