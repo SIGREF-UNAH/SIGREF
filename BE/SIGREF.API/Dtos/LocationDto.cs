822b0ac19d4689f4d6164c2a978ae8126a2d0e89
AnthonyEMF <anthony07miranda@gmail.com>
2025-09-28 22:07:14 -0600

    Merge main to crud-healthcare-backup

--- BE/SIGREF.API/Dtos/LocationDto.cs
+++ BE/SIGREF.API/Dtos/LocationDto.cs
@@ -23,28 +23,3 @@ public class LocationDto
     
     public DateTime? LastUpdated { get; set; }
 }
-<<<<<<< HEAD
-=======
-
-public class AddressDto
-{
-    public string? Use { get; set; }
-    public string? Type { get; set; }
-    public string? Text { get; set; }
-    public List<string> Line { get; set; } = new();
-    public string? City { get; set; }
-    public string? District { get; set; }
-    public string? State { get; set; }
-    public string? PostalCode { get; set; }
-    //TODO: usar libreria en frontend 
-    public string? Country { get; set; }
-}
-
-public class ContactPointDto
-{
-    public string? System { get; set; }  // phone, email, fax, etc.
-    public string? Value { get; set; }
-    public string? Use { get; set; }     // home, work, mobile, etc.
-    public int? Rank { get; set; }
-}
->>>>>>> main
