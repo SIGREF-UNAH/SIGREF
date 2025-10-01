5e42af18eaf4ff3fb094223bc6c5884e50534c1d
Pineda04 <carlosovidiopineda8@gmail.com>
2025-10-01 14:57:28 -0600

    Create location details UI and connect to backend

--- FE/src/features/locations/routers/LocationsRouter.tsx
+++ FE/src/features/locations/routers/LocationsRouter.tsx
@@ -1,15 +1,21 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import CreateLocationPage from "../pages/create/CreateLocationPage";
 import LocationListPage from "../pages/list/LocationListPage";
+import LocationDetailsPage from "../pages/list/LocationDetailsPage";
 
 const LocationsRouter = () => {
   return (
     <Routes>
+     <Route path="*" element={<Navigate to="list" replace />} />
+
       {/* Ruta principal para listar ubicaciones */}
       <Route path="list" element={<LocationListPage />} />
       
       {/* Ruta para crear una ubicación */}
-      <Route path="create" element={<CreateLocationPage />} />      
+      <Route path="create" element={<CreateLocationPage />} />  
+
+      {/* Ruta para ver detalles de una ubicación */}
+      <Route path="/details/:id" element={<LocationDetailsPage />} />    
     </Routes>
   );
 };
