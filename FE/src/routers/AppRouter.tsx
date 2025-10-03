0af5ed6b9298c0f48f10473672312e3abfd13cc1
MichaelGald <157521897+MichaelGald@users.noreply.github.com>
2025-10-02 19:43:52 -0600

    feat: Implementar layout principal con ProLayout y menús dinámicos por rol usando Keycloak (#88)

--- FE/src/routers/AppRouter.tsx
+++ FE/src/routers/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router";
 import { Home } from "../features/auth/pages";
+import { Layout } from "../shared/components";
+
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
   );
-};  
+};
