9fdc5864d8388a94f1c02eb8f0ff4cb92de07213
Anthony Edward Miranda Fuentes <157448636+AnthonyEMF@users.noreply.github.com>
2025-11-18 01:05:44 -0600

    Vista para generar un reporte (#181)
    * Agregar vista para generar un reporte con datos mock
    
    * Ajustes en los botones

---
+++ FE/src/features/reports/store/reportsData.ts
@@ -0,0 +1,62 @@
+export const reportData = [
+  {
+    key: "1",
+    fecha: "01-03-2025",
+    recibo: "#1235645",
+    serie: "Serie C",
+    auxiliar: "Leonardo Davici",
+    paciente: "Juan Carlos Pérez Domingos",
+    servicio: "Rayos X",
+    mayorEdad: "X",
+    monto: "Exonerado",
+  },
+  {
+    key: "2",
+    fecha: "01-03-2025",
+    recibo: "#1235645",
+    serie: "Serie C",
+    auxiliar: "Leonardo Davici",
+    paciente: "Juan Carlos Pérez Domingos",
+    servicio: "Rayos X",
+    mayorEdad: "X",
+    monto: "Exonerado",
+  },
+  {
+    key: "3",
+    fecha: "01-03-2025",
+    recibo: "#1235645",
+    serie: "Serie C",
+    auxiliar: "Leonardo Davici",
+    paciente: "Juan Carlos Pérez Domingos",
+    servicio: "Rayos X",
+    mayorEdad: "NO",
+    monto: "Exonerado",
+  },
+  {
+    key: "4",
+    fecha: "01-03-2025",
+    recibo: "#1235645",
+    serie: "Serie C",
+    auxiliar: "Leonardo Davici",
+    paciente: "Juan Carlos Pérez Domingos",
+    servicio: "Rayos X",
+    mayorEdad: "X",
+    monto: "300.00",
+  },
+  {
+    key: "5",
+    fecha: "01-03-2025",
+    recibo: "#1235645",
+    serie: "Serie C",
+    auxiliar: "Leonardo Davici",
+    paciente: "Juan Carlos Pérez Domingos",
+    servicio: "Rayos X",
+    mayorEdad: "X",
+    monto: "200.00",
+  },
+];
+
+export const locations = ['Laboratorio', 'Clinica', 'Consulta Externa'];
+export const users = ['Anthony Miranda', 'María González', 'Juan Perez'];
+export const healthcares = ['Examen de Sangre', 'Radiografia Dental', 'Rayos X', 'Cirujia Craneal'];
+export const periods = ['Mes Actual', 'Mes Anterior', 'Último Trimestre', 'Año Actual', 'Personalizado'];
