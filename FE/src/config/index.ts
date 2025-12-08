da0a2cf466527731d7c7aa680912aac696b0be78
Anthony Edward Miranda Fuentes <157448636+AnthonyEMF@users.noreply.github.com>
2025-12-07 23:46:05 -0600

    Ocultar opciones de Layout y atajos por rol (#235)
    Closed #233

--- FE/src/config/index.ts
+++ FE/src/config/index.ts
@@ -1,4 +1,4 @@
-export * from './routes';
+export * from './layout-options';
 export * from './providers/AbilityProvider';
 export * from './providers/AntdProvider';
 export * from './providers/ShortcutsProvider';
