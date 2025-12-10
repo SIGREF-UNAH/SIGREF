f9ea6d99d00b3f28374a358757a6bcdbf4b09e57
AnthonyEMF <anthony07miranda@gmail.com>
2025-12-10 00:06:39 -0600

    Hookerizacion de EventHistoryPage y optimizacion de archivos

--- FE/src/config/index.ts
+++ FE/src/config/index.ts
@@ -1,5 +1,5 @@
 export * from './layout-options';
 export * from './providers/AbilityProvider';
-export * from './providers/AntdProvider';
+export * from './providers/AntDesignProvider';
 export * from './providers/ShortcutsProvider';
 export * from './shortcuts';
--- FE/src/config/providers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './AbilityProvider';
-export * from './AntdProvider';
-export * from './ShortcutsProvider';
